773743bb932e19573f1049ab61d53bb53c11d932
NGdre <100stepstosuccess@gmail.com>
2026-01-17 16:35:49 +0300

    [FEAT]: solve "indexOf" problem

---
+++ problems/index-of.ts
@@ -0,0 +1,13 @@
+type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
+    ? true
+    : false;
+
+type IndexOf<T, U, I extends unknown[] = []> = T extends [
+  infer F,
+  ...infer Rest,
+]
+  ? Equal<F, U> extends true
+    ? I["length"]
+    : IndexOf<Rest, U, [...I, never]>
+  : -1;
