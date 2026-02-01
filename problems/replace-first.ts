5601a8da293c4ce89803c6e85a0e2da06f93e6b6
NGdre <100stepstosuccess@gmail.com>
2026-02-01 22:23:05 +0300

    [FEAT]: solve "replace first" problem

---
+++ problems/replace-first.ts
@@ -0,0 +1,8 @@
+type ReplaceFirst<T extends readonly unknown[], S, R> = T extends [
+  infer F,
+  ...infer Rest,
+]
+  ? F extends S
+    ? [R, ...Rest]
+    : [F, ...ReplaceFirst<Rest, S, R>]
+  : T;
